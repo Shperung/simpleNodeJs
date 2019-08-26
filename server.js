f60e35398e657a13fdac7d280439d1f20937f4ab
Viktor Krav4uk Home <viktorkrav4uk87@gmail.com>
2019-08-26 18:11:21 +0300

    rewrite server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = (req, res) => {
+// простий варіант
+/*const server = (req, res) => {
 	res.writeHead(200, {
 		'Content-type':'text/html;charset=utf-8',
 	});
@@ -15,4 +16,30 @@ const server = (req, res) => {
 const app = http.createServer(server);
 
 app.listen(8080);
-console.log('Listening on 8080...');
+console.log('Listening on 8080...');*/
+
+// варіант на подіях
+
+const server = http.createServer().listen(8080);
+
+server.on('request', (req, res) => {
+	res.writeHead(200, {
+		'Content-type':'text/html;charset=utf-8',
+	});
+
+	fs.readFile('index.html', (err, content) => {
+		res.write(decodeURIComponent(content));
+		res.end();
+	});
+});
+
+server.on('request', (req, res) => {
+	console.log('req-:', req.method, req.url);
+});
+
+server.on('listening', () => {
+	console.log('Listening on 8080......');
+});
+
+
+
